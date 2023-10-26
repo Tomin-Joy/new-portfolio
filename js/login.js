1bd0fd7f3b4dffe2fc0176733f6ddce7c61f9596
Tomin-Joy <tominjk007@gmail.com>
2023-10-26 21:03:57 +0530

    [added] login page

---
+++ js/login.js
@@ -0,0 +1,10 @@
+function spanner(){
+    for (let index = 0; index < 260; index++) {
+        login_section.insertBefore(document.createElement("span"),login_section.children[0])
+        
+    }
+}
+
+function loginUsr(){
+    login_section.style.display="none"
+}
